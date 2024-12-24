768bf60926738b7f148bfc1c0e3cab92c2f586de
Raid-the-XPRT <raltamimi0001@stu.kau.edu.sa>
2024-12-24 03:43:32 -0800

    the first push

---
+++ CX204-LAB6/CX204-LAB6.srcs/sources_1/new/inst_mem.sv
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/24/2024 03:42:27 AM
+// Design Name: 
+// Module Name: inst_mem
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module inst_mem(
+
+    );
+endmodule
+hello amr :)
