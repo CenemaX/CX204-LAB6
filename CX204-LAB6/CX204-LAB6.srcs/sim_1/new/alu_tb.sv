e99ba352971f127c2e30f8c6ccdb0e5e95ae548a
Raid-the-XPRT <raltamimi0001@stu.kau.edu.sa>
2024-12-25 06:45:22 -0800

    add finished alu module and test bench

---
+++ CX204-LAB6/CX204-LAB6.srcs/sim_1/new/alu_tb.sv
@@ -0,0 +1,91 @@
+`timescale 1ns / 1ps
+
+/*
+module alu(
+    input logic [31:0]op1,
+    input logic [31:0]op2,
+    input logic [3:0]alu_ctrl,
+    output logic [31:0]alu_result,
+    output logic zero
+    );
+    
+    logic [31:0]uop1 = $unsigned(op1);
+    logic [31:0]uop2 = $unsigned(op2);
+    
+    always@(*)begin
+        case (alu_ctrl)
+            0   :   alu_result<=op1+op2;
+            8   :   alu_result<=op1-op2;
+            4   :   alu_result<=op1^op2;
+            6   :   alu_result<=op1|op2;
+            7   :   alu_result<=op1&op2;
+            1   :   alu_result<=op1<<op2;
+            5   :   alu_result<=op1>>op2;
+            13  :   alu_result<=op1>>>op2;
+            2   :   alu_result<= (op1<op2)      ? 1 : 0;
+            3   :   alu_result<= (uop1<uop2)    ? 1 : 0;
+        endcase
+    end 
+                
+endmodule
+*/
+
+module alu_tb;
+
+logic [31:0]op1;
+logic [31:0]op2;
+logic [3:0]alu_ctrl;
+logic [31:0]alu_result;
+logic zero;
+
+
+alu alu1(.op1(op1),.op2(op2),.alu_ctrl(alu_ctrl),.alu_result(alu_result),.zero(zero));
+
+initial begin
+    #10
+    op1=32'b00000000000000000000000000000000;
+    op2=32'b00000000000000000000000000000000;
+    alu_ctrl=4'd0;
+    #10
+    op1=32'b00000000000000000000000000000111;
+    op2=32'b00000000000000000000000000001000;
+    alu_ctrl=4'd0;
+    #10
+    op1=32'b11111111111111111111111111111111;
+    op2=32'b11111111111111111111111111111111;
+    alu_ctrl=4'd8;
+    #10
+    op1=32'b10101010101010101010101010101111;
+    op2=32'b01010101010101010101010101011111;
+    alu_ctrl=4'd4;
+    #10
+    op1=32'b10101010101010101010101010101111;
+    op2=32'b01010101010101010101010101010000;
+    alu_ctrl=4'd6;
+    #10
+    op1=32'b11111111111111111111111111111111;
+    op2=32'b10101010101010101010101101010101;
+    alu_ctrl=4'd7;
+    #10
+    op1=32'b00000000000000000000000000000011;
+    op2=32'b00000000000000000000000000000100;
+    alu_ctrl=4'd1;
+    #10
+    op1=32'b01100000000000000000000000000000;
+    op2=32'b00000000000000000000000000000100;
+    alu_ctrl=4'd5;
+    #10
+    op1=32'b11111111111111111111111111111111;
+    op2=32'b00000000000000000000000000001000;
+    alu_ctrl=4'd13;
+    #10
+    op1=32'b00000000000000000000000000000011;
+    op2=32'b00000000000000000000000000000100;
+    alu_ctrl=4'd2;
+    #10;
+    op1=32'b11111111111111111111111111111111;
+    op2=32'b00000000000000000000000000000000;
+    alu_ctrl=4'd3;
+    #10;
+    end
+endmodule
--- CX204-LAB6/CX204-LAB6.srcs/sources_1/new/alu.sv
+++ CX204-LAB6/CX204-LAB6.srcs/sources_1/new/alu.sv
@@ -1,26 +1,34 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/24/2024 11:15:39 PM
-// Design Name: 
-// Module Name: alu
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module alu(
-
+    input logic [31:0]op1,
+    input logic [31:0]op2,
+    input logic [3:0]alu_ctrl,
+    output logic [31:0]alu_result,
+    output logic zero
     );
+    
+    logic [31:0]uop1; 
+    logic [31:0]uop2; 
+    
+    assign uop1 = $unsigned(op1);
+    assign uop2 = $unsigned(op2);
+    always@(*)begin
+        case (alu_ctrl)
+            0   :   alu_result<=op1+op2;
+            8   :   alu_result<=op1-op2;
+            4   :   alu_result<=op1^op2;
+            6   :   alu_result<=op1|op2;
+            7   :   alu_result<=op1&op2;
+            1   :   alu_result<=op1<<op2[4:0];
+            5   :   alu_result<=op1>>op2[4:0];
+            13  :   alu_result<=$signed(op1) >>> op2[4:0];
+            2   :   alu_result<= (op1<op2)      ? 32'h0001 : 32'h0000;
+            3   :   alu_result<= (uop1<uop2)    ? 32'h0001 : 32'h0000;
+            default: alu_result<=0;
+        endcase
+        
+        zero <= op2==32'h0000;
+    end 
+                
 endmodule
